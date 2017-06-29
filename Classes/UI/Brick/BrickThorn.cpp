aa9ed1982e0eb6dc82bb81260c82ea4ef124a5da
blueberrybeing <2468738829@qq.com>
2017-06-29 16:54:25 +0800

    update die

---
+++ Classes/UI/Brick/BrickThorn.cpp
@@ -0,0 +1,9 @@
+//
+//  BrickThorn.cpp
+//  Minos
+//
+//  Created by jingjing on 2017/6/29.
+//
+//
+
+#include "BrickThorn.hpp"
---
+++ Classes/UI/Brick/BrickThorn.hpp
@@ -0,0 +1,14 @@
+//
+//  BrickThorn.hpp
+//  Minos
+//
+//  Created by jingjing on 2017/6/29.
+//
+//
+
+#ifndef BrickThorn_hpp
+#define BrickThorn_hpp
+
+#include <stdio.h>
+
+#endif /* BrickThorn_hpp */
