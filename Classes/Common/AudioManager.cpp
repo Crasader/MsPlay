733c2d9749ef07b65295c78786a333fd66db39a1
blueberrybeing <2468738829@qq.com>
2017-07-03 14:05:28 +0800

    添加Audio处理

---
+++ Classes/Common/AudioManager.cpp
@@ -0,0 +1,9 @@
+//
+//  AudioManager.cpp
+//  Minos
+//
+//  Created by jingjing on 2017/6/29.
+//
+//
+
+#include "AudioManager.hpp"
---
+++ Classes/Common/AudioManager.hpp
@@ -0,0 +1,14 @@
+//
+//  AudioManager.hpp
+//  Minos
+//
+//  Created by jingjing on 2017/6/29.
+//
+//
+
+#ifndef AudioManager_hpp
+#define AudioManager_hpp
+
+#include <stdio.h>
+
+#endif /* AudioManager_hpp */
